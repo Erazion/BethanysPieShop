89e9c16a3d5c71175ef3c0639a799204af27dd41
Bouverot Luc <luc.bouverot@consultant.volvo.com>
2019-06-06 11:45:03 +0200

    First commit

---
+++ BethanysPieShop/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BethanysPieShop
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add the Context, using SQL Server
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // AddTransient means that every time a new IPieRepository is called, it creates a new instance
+            services.AddTransient<IPieRepository, PieRepository>();
+            services.AddTransient<IFeedbackRepository, FeedbackRepository>();
+            // AddSingleton means that only one single instance will be created of the specified type
+            // AddScope always return the same instance, but if it is out of scope a new instance is created
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();    // Yellow screen of death won't be displayed
+            app.UseStatusCodePages();           // Show status of request
+            app.UseStaticFiles();               // Return the static file (image, js file, ...)
+            // app.UseMvcWithDefaultRoute();    // Routing
+            app.UseAuthentication();            // Authentication
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
